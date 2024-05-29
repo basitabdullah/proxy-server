0faf4383b20ff4bc6ee592e035376c2f42b6dca0
fahim <basitabdullah1122@gmail.com>
2024-05-29 13:17:30 +0530

    Added Offers Api

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ res.json(await data)
 }) 
 
 
+app.get("/offers", async (req, res) => {
+  const response = await fetch(
+    "https://inrdeals.com/api/v1/coupon-feed?token=7d93f1be383789a926af8abea04225ad421ece1c&id=maa443089855"
+  );
+  const data = await response.json();
+  res.json(await data);
+}); 
+
+
+
 app.listen(3000,()=>{
     console.log("Listening On port 3000");
 })
